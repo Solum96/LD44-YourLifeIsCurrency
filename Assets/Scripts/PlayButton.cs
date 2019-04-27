1a049aac096b2fed8c7bc470e3d7f94d8a63b606
Solum96 <nils.solumfaeste@gmail.com>
2019-04-27 21:13:58 +0200

    Add Main Menu

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+   
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReadInst()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
