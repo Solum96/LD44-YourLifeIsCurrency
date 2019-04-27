e5959b19cbcf93ad3031554879e169a8f0a0cc28
Solum96 <nils.solumfaeste@gmail.com>
2019-04-27 16:31:24 +0200

    add Game over text and restartoption

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿//(We have nothing to lose but our chains)
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     public Player player;
-
+    public GameObject GameOverText;
     void Update()
     {
+        //Game Over Text och Omstart 
+        GameOverText.SetActive(player==null);
       if (player == null)
         {
+
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
             {
                 Oxygen.ResetOxygen();
