a59ebe1274e9b343281dacee0f17e840557f88d6
Solum96 <nils.solumfaeste@gmail.com>
2019-04-27 20:38:06 +0200

    Add ScoreTimer

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text _text;
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+    float Timer = 0.0f;
+    void Update()
+    {
+        
+        Timer += Time.deltaTime; //Time.deltaTime will increase the value with 1 every second.
+        _text.text = Timer.ToString("0.00");
+    }
+}
